6c73ecbdfbb4e5e234a79fb027d5833b01f6677b
ella choy <egal21de@gmail.com>
2022-02-11 17:01:09 +0100

    alert

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 console.log('hi')
 	
 // alert(1+1);
-Math.pow(3,2);       // 9,   3의 2승 
-Math.round(10.6);    // 11,  10.6을 반올림
-Math.ceil(10.2);     // 11,  10.2를 올림
-Math.floor(10.6);    // 10,  10.6을 내림
-Math.sqrt(9);        // 3,   3의 제곱근
-Math.random();       // 0부터 1.0 사이의 랜덤한 숫자
+// Math.pow(3,2);       // 9,   3의 2승 
+// Math.round(10.6);    // 11,  10.6을 반올림
+// Math.ceil(10.2);     // 11,  10.2를 올림
+// Math.floor(10.6);    // 10,  10.6을 내림
+// Math.sqrt(9);        // 3,   3의 제곱근
+// Math.random();       // 0부터 1.0 사이의 랜덤한 숫자
