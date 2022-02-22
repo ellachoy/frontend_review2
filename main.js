8149d9c8cecafd9a92dc7a13fe611008b7c761a3
ella choy <egal21de@gmail.com>
2022-02-22 11:45:39 +0100

    while

--- main.js
+++ main.js
@@ -77,10 +77,22 @@ let i = 0;
 // 종료조건으로 i의 값이 10보다 작다면 true, 같거나 크다면 false가 된다.
 while(i < 10){
     // 반복이 실행될 때마다 coding everybody <br />이 출력된다. <br /> 줄바꿈을 의미하는 HTML 태그
-    document.write('coding everybody <br />');
+    // document.write('coding everybody <br />');
+     document.write('* <br />');
     // i의 값이 1씩 증가한다.
-    i++
+    // i++ 
+    i=i+1;
 }
 for(let i = 0; i < 10; i++){
     document.write('coding everybody'+i+'<br />');
 }
+// // 0부터 9까지 변수 i에 순차적으로 값을 할당        
+// for(var i = 0; i < 10; i++){
+//     // 0부터 9까지의 변수를 j의 값에 순차적으로 할당
+//     for(var j = 0; j < 10; j++){    
+//         // i와 j의 값을 더한 후에 출력
+//         // String은 숫자인 i와 j의 데이터 타입을 문자로 형태를 변환하는 명령이다. 
+//         // String()을 제거하고 실행해보면 의미가 좀 더 분명하게 드러날 것이다.
+//         document.write(String(i)+String(j)+'<br />');
+//     }
+// }
