6c7cf4bb1ba697d41a6147e3c05b4b944fe34dc5
ella choy <egal21de@gmail.com>
2022-02-23 11:39:50 +0100

    break

--- main.js
+++ main.js
@@ -116,6 +116,10 @@ for(var x = 0; x < 5; x++){
     document.write('<br />');
 }
 document.write('<br />');
+
 for(var i = 0; i < 10; i++){
+    if(i===5){
+        break;
+    }
     document.write('coding everybody'+i+'<br />');
 }
