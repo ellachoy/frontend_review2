4866a0a053714dd7b4680813bac0980a3ec39854
ella choy <egal21de@gmail.com>
2022-03-03 20:43:31 +0100

    callback2

--- main.js
+++ main.js
@@ -319,4 +319,22 @@ function cal(mode){
     return funcs[mode];
 }
 alert(cal('plus')(2,1));
-alert(cal('minus')(2,1));   
+alert(cal('minus')(2,1));  
+
+var process = [
+    function(input){ return input + 10;},
+    function(input){ return input * input;},
+    function(input){ return input / 2;}
+];
+var input = 1;
+for(var i = 0; i < process.length; i++){
+    input = process[i](input);
+}
+alert(input);
+
+function sortNumber(a,b){
+    // 위의 예제와 비교해서 a와 b의 순서를 바꾸면 정렬순서가 반대가 된다.
+    return b-a;
+}
+var numbers = [20, 10, 9,8,7,6,5,4,3,2,1];
+alert(numbers.sort(sortNumber)); // array, [20,10,9,8,7,6,5,4,3,2,1]
